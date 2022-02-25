99a94efaa86fe852661c274ca3f576244bd12ba5
SirSimon04 <simi@engelnetz.de>
2022-02-26 00:02:29 +0100

    add files to print points

---
+++ lib/screens/drawing_screen/drawing_screen.dart
@@ -0,0 +1,156 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+import 'components/drawn_line.dart';
+
+class DrawingScreen extends StatefulWidget {
+  const DrawingScreen({Key? key}) : super(key: key);
+
+  @override
+  _DrawingScreenState createState() => _DrawingScreenState();
+}
+
+class _DrawingScreenState extends State<DrawingScreen> {
+  final GlobalKey _globalKey = new GlobalKey();
+  List<DrawnLine> lines = <DrawnLine>[];
+  late DrawnLine line;
+  Color selectedColor = Colors.black;
+  double selectedWidth = 5.0;
+
+  StreamController<List<DrawnLine>> linesStreamController =
+      StreamController<List<DrawnLine>>.broadcast();
+  StreamController<DrawnLine> currentLineStreamController =
+      StreamController<DrawnLine>.broadcast();
+
+  Future<void> save() async {
+    // TODO
+  }
+
+  Future<void> clear() async {
+    // TODO
+  }
+
+  void onPanStart(DragStartDetails details) {
+    print('User started drawing');
+    final box = context.findRenderObject() as RenderBox;
+    final point = box.globalToLocal(details.globalPosition);
+    print(point);
+  }
+
+  void onPanUpdate(DragUpdateDetails details) {
+    final box = context.findRenderObject() as RenderBox;
+    final point = box.globalToLocal(details.globalPosition);
+    print(point);
+  }
+
+  void onPanEnd(DragEndDetails details) {
+    print('User ended drawing');
+  }
+
+  GestureDetector buildCurrentPath(BuildContext context) {
+    return GestureDetector(
+      onPanStart: onPanStart,
+      onPanUpdate: onPanUpdate,
+      onPanEnd: onPanEnd,
+      child: RepaintBoundary(
+        child: Container(
+          color: Colors.transparent,
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          // CustomPaint widget will go here
+        ),
+      ),
+    );
+  }
+
+  // Widget buildAllPaths(BuildContext context) {
+  //   // TODO
+  // }
+  //
+  // Widget buildStrokeToolbar() {
+  //   // TODO
+  // }
+
+  Widget buildStrokeButton(double strokeWidth) {
+    return GestureDetector(
+      onTap: () {
+        selectedWidth = strokeWidth;
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Container(
+          width: strokeWidth * 2,
+          height: strokeWidth * 2,
+          decoration: BoxDecoration(
+              color: selectedColor, borderRadius: BorderRadius.circular(20.0)),
+        ),
+      ),
+    );
+  }
+
+  // Widget buildColorToolbar() {
+  //   // TODO
+  // }
+
+  Widget buildColorButton(Color color) {
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: FloatingActionButton(
+        mini: true,
+        backgroundColor: color,
+        child: Container(),
+        onPressed: () {
+          setState(() {
+            selectedColor = color;
+          });
+        },
+      ),
+    );
+  }
+
+  Widget buildSaveButton() {
+    return GestureDetector(
+      onTap: save,
+      child: CircleAvatar(
+        child: Icon(
+          Icons.save,
+          size: 20.0,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+
+  Widget buildClearButton() {
+    return GestureDetector(
+      onTap: clear,
+      child: CircleAvatar(
+        child: Icon(
+          Icons.create,
+          size: 20.0,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.yellow[50],
+      body: Stack(
+        children: [
+          buildCurrentPath(context),
+        ],
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    linesStreamController.close();
+    currentLineStreamController.close();
+    super.dispose();
+  }
+}
