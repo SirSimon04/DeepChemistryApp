c888b2ac1df4f5fc218042f5296e511946557a9c
SirSimon04 <simi@engelnetz.de>
2022-02-26 02:43:56 +0100

    add bottom nav bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,61 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_deep_chemistry/screens/drawing_screen/drawing_screen.dart';
 
+import 'screens/testing_screen/testing_screen.dart';
+
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: "DeepChemistry",
       debugShowCheckedModeBanner: false,
-      home: DrawingScreen(),
+      home: MyApp(),
     );
+  }
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _pages = [DrawingScreen(), TestingScreen()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.border_color),
+            label: "Zeichnen",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.image),
+            label: "Testen",
+          ),
+        ],
+        onTap: _onItemTapped,
+        currentIndex: _selectedIndex,
+      ),
+      body: _pages[_selectedIndex],
+    );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
---
+++ lib/screens/testing_screen/testing_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TestingScreen extends StatelessWidget {
+  const TestingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Hier kann etwas getestet werden"),
+    );
+  }
+}
