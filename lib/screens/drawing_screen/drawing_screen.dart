4201c27df807cac29dfcf942ec4826c61c5070cf
Simon Engel <74311832+SirSimon04@users.noreply.github.com>
2022-03-29 19:01:16 +0200

    Showing spinner on top while sending data

--- lib/screens/drawing_screen/drawing_screen.dart
+++ lib/screens/drawing_screen/drawing_screen.dart
@@ -1,14 +1,7 @@
-import 'dart:async';
 import 'dart:math';
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_deep_chemistry/screens/drawing_screen/components/custom_draw_widget.dart';
-import 'package:image_gallery_saver/image_gallery_saver.dart';
 import '../../widgets/loader.dart';
-import 'components/drawn_line.dart';
-import 'components/sketcher.dart';
-import "package:screenshot/screenshot.dart";
 
 class DrawingScreen extends StatefulWidget {
   const DrawingScreen({Key? key}) : super(key: key);
